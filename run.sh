f6adee6cb703ce1e35f6ad7539d97af4f631ac91
Rohan Mishra <67382877+romishra12@users.noreply.github.com>
2024-09-09 10:38:59 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl "https://www.google.com/maps/d/u/0/kml?mid=1SJVOiniy-JAtO3Um-Tyyk3gvC1GET7s&forcekml=a" --output data.kml
+curl "https://www.google.com/maps/d/kml?mid=1SJVOiniy-JAtO3Um-Tyyk3gvC1GET7s&forcekml=1" --output data.kml
